f0d03e4b02d8b85b83c6d4f327c8f4baeb931fbc
werliton <werlitoncarlos@gmail.com>
2019-10-26 21:17:11 -0300

    Refactoring Link Component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
 
-class Link extends Component{
-    render(){
-        return (
-            <>
-                <div>
-                    {this.props.link.description} ({this.props.link.url})
-                    </div>
-            </>
-        )
-    }
-}
-
-export default Link
+export const Link = ({ link }) => (
+  <>
+    <div>
+      {link.description} ({link.url})
+    </div>
+  </>
+);
