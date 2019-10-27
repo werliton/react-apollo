cd22d2bbb3f400e5cbd76dd9765533aca0fc6924
werliton <werlitoncarlos@gmail.com>
2019-10-26 21:31:46 -0300

    Creating query graphql in LinkList

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
-import { Link } from './Link'
+import { Link } from "./Link";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
-class LinkList extends Component{
-    render(){
-        const linksToRender = [
-            {
-                id: '1',
-                description: 'Prisma turns your database into a GraphQL API 😎',
-                url: 'https://www.prismagraphql.com',
-              },
-              {
-                id: '2',
-                description: 'The best GraphQL client',
-                url: 'https://www.apollographql.com/docs/react/',
-              }
-        ]
+class LinkList extends Component {
+  render() {
+    const FEED_QUERY = gql`
+      {
+        feed {
+          links {
+            id
+            createdAt
+            url
+            description
+          }
+        }
+      }
+    `;
+    const linksToRender = [
+      {
+        id: "1",
+        description: "Prisma turns your database into a GraphQL API 😎",
+        url: "https://www.prismagraphql.com"
+      },
+      {
+        id: "2",
+        description: "The best GraphQL client",
+        url: "https://www.apollographql.com/docs/react/"
+      }
+    ];
 
-        return (
-            <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
-        )
-    }
+    return (
+      <Query query={FEED_QUERY}>
+        {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
+      </Query>
+    );
+  }
 }
 
-export default LinkList
+export default LinkList;
