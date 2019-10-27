39877a55b84d243bc704c24eda80758d5e724546
werliton <werlitoncarlos@gmail.com>
2019-10-26 21:36:17 -0300

    Finish Queries with Apollo Client

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -32,7 +32,19 @@ class LinkList extends Component {
 
     return (
       <Query query={FEED_QUERY}>
-        {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map(link => (
+                <Link key={link.id} link={link} />
+              ))}
+            </div>
+          );
+        }}
       </Query>
     );
   }
